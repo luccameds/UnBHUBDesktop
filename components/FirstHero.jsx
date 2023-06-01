0e88c663b97c8b26fdcc4d6fa45dadf4b700488e
Lucca Medeiros <luccameds@gmail.com>
2023-06-01 19:21:43 -0300

    Created the FirstHero in components

---
+++ components/FirstHero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+
+const FirstHero = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default FirstHero;
