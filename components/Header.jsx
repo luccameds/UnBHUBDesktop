69a4e38f25a9d11175b6d812b65ae8ec7eb3c683
medeiros <luccameds@gmail.com>
2024-03-21 20:38:29 -0300

    Feat: Updated the responsive landingPage.

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="flex flex-1 flex-row px-[90px] py-[30px]">
-      <img src="./logo.svg" alt="logo" className="flex h-[30px] flex-1" />
-      <div className="flex w-[900px] flex-1 flex-row justify-between font-light text-black max-md:hidden">
-        <p>Depósito</p>
-        <p>Dashboard</p>
-        <p>Empresa</p>
-        <p>Preços</p>
+    <div className="flex flex-1 flex-row items-center px-[90px] py-[30px]">
+      <img
+        src="./logo.jpg"
+        alt="logo"
+        className="flex h-[60px] flex-1 object-contain"
+      />
+      <div className="flex w-[900px] flex-1 flex-row justify-between font-light text-black max-lg:hidden">
+        <p>Home</p>
+        <p>Quem somos</p>
+        <p>Termos</p>
+        <p>Notícias</p>
       </div>
     </div>
   );
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="px-[150px] py-[80px]">
+    <div className="px-[150px] py-10">
       <img
         src="./asterics.svg"
         alt="logo"
         className="my-[42px] w-[60px] justify-self-start"
       />
       <h1 className="text-[50px] text-[#1D1D1C]">
-        Todas as{' '}
+        Descomplicando as{' '}
         <h1 className="text-[50px] font-bold text-[#1D1D1C]">experiências </h1>
-        no novo cartão de crédito
+        na universidade
       </h1>
-      <img src="./arrow.svg" alt="arrow" className="my-[60px]" />
+      <img src="./arrow.svg" alt="arrow" className="my-[30px]" />
       <p className="w-[450px] text-[#1D1D1C]">
-        Transferências simples, pagamentos de contas de serviços públicos,
-        extrato funcional, configurações do cartão, para as quais costumava ser
-        necessário ir até a agência ou acessar o banco online.
+        Desde organizar sua agenda até conectar-se com colegas para projetos
+        colaborativos, nossa plataforma oferece uma experiência completa e
+        intuitiva. Explore novos caminhos de aprendizado, descubra recursos
+        úteis e otimize sua rotina universitária conosco. Junte-se a nós e
+        liberte todo o potencial da sua jornada acadêmica!
       </p>
-      <div className="my-[30px] flex flex-row">
-        <button className="mx-[39px] h-[33px] w-[167px] rounded-[100px] bg-[#11253E]">
-          Solicitar um Cartão
+      <div className="my-20 flex flex-row">
+        <button className="rounded-[100px] bg-[#1D1D1C] px-5 py-3">
+          Criar conta
         </button>
-        <button className="flex h-[33px] flex-row items-center justify-between rounded-[100px] bg-white px-[15px]">
+        <button className="flex flex-row items-center justify-between rounded-[100px] bg-white px-[15px]">
           <p className="mx-[10px] text-black">Como funciona</p>
           <img src="./arrowbutton.svg" alt="arrowbutton" />
         </button>
